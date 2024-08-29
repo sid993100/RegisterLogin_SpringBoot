eca490fddc08636288360883ca238bef81fda6d0
Siddharth Pathak <sid993100@gmail.com>
2024-08-29 19:28:35 +0530

    first commit

---
+++ src/main/java/com/sid993100/RegisterLogin_SpringBoot/RegisterLoginSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sid993100.RegisterLogin_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterLoginSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterLoginSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sid993100/RegisterLogin_SpringBoot/RegisterLoginSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid993100.RegisterLogin_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterLoginSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
